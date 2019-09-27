66777f87a2571f5be0940af7cf17addab2e1b21b
Brian Bidulock <bidulock@openss7.org>
2019-09-27 17:37:31 -0600

    handle options processing

--- src/pacana.c
+++ src/pacana.c
@@ -1,5 +1,465 @@
+/*****************************************************************************
+
+ Copyright (c) 2010-2019  Monavacon Limited <http://www.monavacon.com/>
+ Copyright (c) 2002-2009  OpenSS7 Corporation <http://www.openss7.com/>
+ Copyright (c) 1997-2001  Brian F. G. Bidulock <bidulock@openss7.org>
+
+ All Rights Reserved.
+
+ This program is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation, version 3 of the license.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program.  If not, see <http://www.gnu.org/licenses/>, or write to the
+ Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+ -----------------------------------------------------------------------------
+
+ U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on
+ behalf of the U.S. Government ("Government"), the following provisions apply
+ to you.  If the Software is supplied by the Department of Defense ("DoD"), it
+ is classified as "Commercial Computer Software" under paragraph 252.227-7014
+ of the DoD Supplement to the Federal Acquisition Regulations ("DFARS") (or any
+ successor regulations) and the Government is acquiring only the license rights
+ granted herein (the license rights customarily provided to non-Government
+ users).  If the Software is supplied to any unit or agency of the Government
+ other than DoD, it is classified as "Restricted Computer Software" and the
+ Government's rights in the Software are defined in paragraph 52.227-19 of the
+ Federal Acquisition Regulations ("FAR") (or any successor regulations) or, in
+ the cases of NASA, in paragraph 18.52.227-86 of the NASA Supplement to the FAR
+ (or any successor regulations).
+
+ -----------------------------------------------------------------------------
+
+ Commercial licensing and support of this software is available from OpenSS7
+ Corporation at a fee.  See http://www.openss7.com/
+
+ *****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include "autoconf.h"
+#endif
+
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 600
+#endif
+
+/** @section Includes
+  * @{ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <sys/poll.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#ifdef _GNU_SOURCE
+#include <getopt.h>
+#endif
+#include <time.h>
+#include <signal.h>
+#include <syslog.h>
+#include <sys/utsname.h>
+
+#include <assert.h>
+#include <locale.h>
+#include <stdarg.h>
+#include <strings.h>
+#include <regex.h>
+#include <wordexp.h>
+#include <execinfo.h>
+
+#include <glib.h>
+
+/** @} */
+
+/** @section Debugging Preamble
+  * @{ */
+
+const char *
+_timestamp(void)
+{
+	static struct timeval tv = { 0, 0 };
+	static struct tm tm = { 0, };
+	static char buf[BUFSIZ];
+	size_t len;
+
+	gettimeofday(&tv, NULL);
+	len = strftime(buf, sizeof(buf) - 1, "%b %d %T", gmtime_r(&tv.tv_sec, &tm));
+	snprintf(buf + len, sizeof(buf) - len - 1, ".%06ld", tv.tv_usec);
+	return buf;
+}
+
+#define XPRINTF(_args...) do { } while (0)
+
+#define DPRINTF(_num, _args...) do { if (options.debug >= _num) { \
+		fprintf(stderr, NAME "[%d]: D: [%s] %12s: +%4d : %s() : ", getpid(), _timestamp(), __FILE__, __LINE__, __func__); \
+		fprintf(stderr, _args); fflush(stderr); } } while (0)
+
+#define EPRINTF(_args...) do { \
+		fprintf(stderr, NAME "[%d]: E: [%s] %12s +%4d : %s() : ", getpid(), _timestamp(), __FILE__, __LINE__, __func__); \
+		fprintf(stderr, _args); fflush(stderr); } while (0)
+
+#define OPRINTF(_num, _args...) do { if (options.debug >= _num || options.output > _num || options.info) { \
+		fprintf(stdout, NAME "[%d]: I: [%s] ", getpid(), _timestamp()); \
+		fprintf(stdout, _args); fflush(stdout); } } while (0)
+
+#define PTRACE(_num) do { if (options.debug >= _num || options.output >= _num) { \
+		fprintf(stderr, NAME "[%d]: T: [%s] %12s +%4d : %s()\n", getpid(), _timestamp(), __FILE__, __LINE__, __func__); \
+		fflush(stderr); } } while (0)
+
+#define CPRINTF(_num, c, _args...) do { if (options.output > _num) { \
+		fprintf(stdout, NAME "[%d]: C: 0x%08lx client (%c) ", getpid(), c->win, c->managed ?  'M' : 'U'); \
+		fprintf(stdout, _args); fflush(stdout); } } while (0)
+
+void
+dumpstack(const char *file, const int line, const char *func)
+{
+	void *buffer[32];
+	int nptr;
+	char **strings;
+	int i;
+
+	if ((nptr = backtrace(buffer, 32)) && (strings = backtrace_symbols(buffer, nptr))) {
+		for (i = 0; i < nptr; i++)
+			fprintf(stderr, NAME "[%d]: E: [%s] %12s +%4d : %s() : <stack> %s\n", getpid(), _timestamp(), file, line, func, strings[i]);
+		fflush(stderr);
+	}
+}
+
+/** @} */
+
+/** @section Definitions of Globals, Enumerations, Structures
+  * @{ */
+
+const char *program = NAME;
+
+typedef enum {
+	CommandDefault = 0,
+	CommandAnalyze,
+	CommandHelp,
+	CommandVersion,
+	CommandCopying,
+} Command;
+
+typedef struct {
+	int debug;
+	int output;
+	Command command;
+} Options;
+
+Options options = {
+	.debug = 0,
+	.output = 1,
+	.command = CommandDefault,
+};
+
+Options defaults = {
+	.debug = 0,
+	.output = 1,
+	.command = CommandDefault,
+};
+
+/** @} */
+
+/** @section Analyze
+  * @{ */
+
+static void
+pac_analyze(void)
+{
+}
+
+/** @} */
+
+/** @section Main
+  * @{ */
+
+static void
+copying(int argc, char *argv[])
+{
+	if (!options.output && !options.debug)
+		return;
+	(void) fprintf(stdout, "\
+--------------------------------------------------------------------------------\n\
+%1$s\n\
+--------------------------------------------------------------------------------\n\
+Copyright (c) 2010-2019  Monavacon Limited <http://www.monavacon.com/>\n\
+Copyright (c) 2002-2009  OpenSS7 Corporation <http://www.openss7.com/>\n\
+Copyright (c) 1997-2001  Brian F. G. Bidulock <bidulock@openss7.org>\n\
+\n\
+All Rights Reserved.\n\
+--------------------------------------------------------------------------------\n\
+This program is free software: you can  redistribute it  and/or modify  it under\n\
+the terms of the  GNU Affero  General  Public  License  as published by the Free\n\
+Software Foundation, version 3 of the license.\n\
+\n\
+This program is distributed in the hope that it will  be useful, but WITHOUT ANY\n\
+WARRANTY; without even  the implied warranty of MERCHANTABILITY or FITNESS FOR A\n\
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.\n\
+\n\
+You should have received a copy of the  GNU Affero General Public License  along\n\
+with this program.   If not, see <http://www.gnu.org/licenses/>, or write to the\n\
+Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.\n\
+--------------------------------------------------------------------------------\n\
+U.S. GOVERNMENT RESTRICTED RIGHTS.  If you are licensing this Software on behalf\n\
+of the U.S. Government (\"Government\"), the following provisions apply to you. If\n\
+the Software is supplied by the Department of Defense (\"DoD\"), it is classified\n\
+as \"Commercial  Computer  Software\"  under  paragraph  252.227-7014  of the  DoD\n\
+Supplement  to the  Federal Acquisition Regulations  (\"DFARS\") (or any successor\n\
+regulations) and the  Government  is acquiring  only the  license rights granted\n\
+herein (the license rights customarily provided to non-Government users). If the\n\
+Software is supplied to any unit or agency of the Government  other than DoD, it\n\
+is  classified as  \"Restricted Computer Software\" and the Government's rights in\n\
+the Software  are defined  in  paragraph 52.227-19  of the  Federal  Acquisition\n\
+Regulations (\"FAR\")  (or any successor regulations) or, in the cases of NASA, in\n\
+paragraph  18.52.227-86 of  the  NASA  Supplement  to the FAR (or any  successor\n\
+regulations).\n\
+--------------------------------------------------------------------------------\n\
+", NAME " " VERSION);
+}
+
+static void
+version(int argc, char *argv[])
+{
+	if (!options.output && !options.debug)
+		return;
+	(void) fprintf(stdout, "\
+%1$s (OpenSS7 %2$s) %3$s\n\
+Written by Brian Bidulock.\n\
+\n\
+Copyright (c) 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019  Monavacon Limited.\n\
+Copyright (c) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009  OpenSS7 Corporation.\n\
+Copyright (c) 1997, 1998, 1999, 2000, 2001  Brian F. G. Bidulock.\n\
+This is free software; see the source for copying conditions.  There is NO\n\
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\
+\n\
+Distributed by OpenSS7 under GNU Affero General Public License Version 3,\n\
+with conditions, incorporated herein by reference.\n\
+\n\
+See `%1$s --copying' for copying permissions.\n\
+", NAME, PACKAGE, VERSION);
+}
+
+static void
+usage(int argc, char *argv[])
+{
+	if (!options.output && !options.debug)
+		return;
+	(void) fprintf(stderr, "\
+Usage:\n\
+    %1$s [-A|--analyze] [options]\n\
+    %1$s {-h|--help} [options]\n\
+    %1$s {-V|--version}\n\
+    %1$s {-C|--copying}\n\
+", argv[0]);
+}
+
+static void
+help(int argc, char *argv[])
+{
+	if (!options.output && !options.debug)
+		return;
+	/* *INDENT-OFF* */
+	(void) fprintf(stdout, "\
+Usage:\n\
+    %1$s [-A|--analyze] [options]\n\
+    %1$s {-h|--help} [options]\n\
+    %1$s {-V|--version}\n\
+    %1$s {-C|--copying}\n\
+Options:\n\
+  Command Options:\n\
+   [-A, --analyze]\n\
+        analyze pacman sync and local databases\n\
+    -h, --help, -?, --?\n\
+        print this usage information and exit\n\
+    -V, --version\n\
+        print version and exit\n\
+    -C, --copying\n\
+        print copying permission and exit\n\
+  General Options:\n\
+    -D, --debug [LEVEL]\n\
+        increment or set debug LEVEL [default: '%2$d']\n\
+    -v, --verbose [LEVEL]\n\
+        increment or set output verbosity LEVEL [default: '%3$d']\n\
+        this option may be repeated.\n\
+", argv[0]
+	, defaults.debug
+	, defaults.output
+	);
+	/* *INDENT-ON* */
+}
+
+static void
+set_defaults(int argc, char *argv[])
+{
+}
+
+static void
+get_defaults(int argc, char *argv[])
+{
+}
 
 int
-main() {
-	return 0;
+main(int argc, char *argv[]) {
+	Command command = CommandDefault;
+
+	setlocale(LC_ALL, "");
+
+	set_defaults(argc, argv);
+
+	while (1) {
+		int c, val;
+		char *endptr = NULL;
+
+#ifdef _GNU_SOURCE
+		int option_index = 0;
+		/* *INDENT-OFF* */
+		static struct option long_options[] = {
+			{"analyze",	no_argument,		NULL, 'A'},
+
+			{"debug",	optional_argument,	NULL, 'D'},
+			{"verbose",	optional_argument,	NULL, 'v'},
+			{"help",	no_argument,		NULL, 'h'},
+			{"version",	no_argument,		NULL, 'V'},
+			{"copying",	no_argument,		NULL, 'C'},
+			{"?",		no_argument,		NULL, 'H'},
+			{ 0, }
+		};
+		/* *INDENT-ON* */
+
+		c = getopt_long_only(argc, argv, "AD::v::hVCH?", long_options,
+				&option_index);
+#else				/* defined _GNU_SOURCE */
+		c = getopt(argc, argv, "ADvhVC?");
+#endif				/* defined _GNU_SOURCE */
+		if (c == -1) {
+			if (options.debug)
+				fprintf(stderr, "%s: done options processing\n", argv[0]);
+			break;
+		}
+		switch (c) {
+		case 0:
+			goto bad_usage;
+		case 'D':	/* -D, --debug [level] */
+			if (options.debug)
+				fprintf(stderr, "%s: increasing debug verbosity\n", argv[0]);
+			if (optarg == NULL) {
+				options.debug++;
+				break;
+			}
+			val = strtoul(optarg, &endptr, 0);
+			if (*endptr)
+				goto bad_option;
+			options.debug = val;
+			break;
+		case 'v':	/* -v, --verbose [level] */
+			if (options.debug)
+				fprintf(stderr, "%s: increasing output verbosity\n", argv[0]);
+			if (optarg == NULL) {
+				options.output++;
+				break;
+			}
+			val = strtoul(optarg, &endptr, 0);
+			if (*endptr)
+				goto bad_option;
+			options.output = val;
+			break;
+		case 'A':	/* -A, --analyze */
+			if (options.command != CommandDefault)
+				goto bad_command;
+			if (command == CommandDefault)
+				command = CommandAnalyze;
+			options.command = CommandAnalyze;
+			break;
+		case 'h':	/* -h, --help */
+		case 'H':	/* -H, --? */
+			command = CommandHelp;
+			break;
+		case 'V':	/* -V, --version */
+			command = CommandVersion;
+			break;
+		case 'C':	/* -C, --copying */
+			command = CommandCopying;
+			break;
+		case '?':
+		default:
+		      bad_option:
+			optind--;
+		      bad_nonopt:
+			if (options.output || options.debug) {
+				if (optind < argc) {
+					fprintf(stderr, "%s: syntax error near '", argv[0]);
+					while (optind < argc)
+						fprintf(stderr, "%s ", argv[optind++]);
+					fprintf(stderr, "'\n");
+				} else {
+					fprintf(stderr, "%s: missing option or argument", argv[0]);
+					fprintf(stderr, "\n");
+				}
+				fflush(stderr);
+			      bad_usage:
+				usage(argc, argv);
+			}
+			exit(2);
+		      bad_command:
+			fprintf(stderr, "%s: only one command option allowed\n", argv[0]);
+			goto bad_usage;
+		}
+	}
+	if (options.debug) {
+		fprintf(stderr, "%s: option index = %d\n", argv[0], optind);
+		fprintf(stderr, "%s: option count = %d\n", argv[0], argc);
+	}
+	if (optind < argc)
+		goto bad_nonopt;
+
+	get_defaults(argc, argv);
+
+	switch (command) {
+	case CommandHelp:
+		if (options.debug)
+			fprintf(stderr, "%s: printing help message\n", argv[0]);
+		help(argc, argv);
+		exit(EXIT_SUCCESS);
+	case CommandVersion:
+		if (options.debug)
+			fprintf(stderr, "%s: printing version message\n", argv[0]);
+		version(argc, argv);
+		exit(EXIT_SUCCESS);
+	case CommandCopying:
+		if (options.debug)
+			fprintf(stderr, "%s: printing copying message\n", argv[0]);
+		copying(argc, argv);
+		exit(EXIT_SUCCESS);
+	case CommandDefault:
+		options.command = command = CommandAnalyze;
+		/* fall thru */
+	case CommandAnalyze:
+		pac_analyze();
+		exit(EXIT_SUCCESS);
+	}
+	EPRINTF("invalid command\n");
+	exit(EXIT_FAILURE);
 }
+
+/** @} */
+
+// vim: set sw=8 tw=80 com=srO\:/**,mb\:*,ex\:*/,srO\:/*,mb\:*,ex\:*/,b\:TRANS foldmarker=@{,@} foldmethod=marker:
