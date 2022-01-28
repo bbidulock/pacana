cb2aeaaf71222f9fbb7ffe5c67f066568d67eee7
Brian Bidulock <bidulock@openss7.org>
2022-01-27 19:42:58 -0700

    update build flags

--- configure.sh
+++ configure.sh
@@ -8,22 +8,16 @@
 
 case "`uname -m`" in
 	i686)
-		CPPFLAGS="-D_FORTIFY_SOURCE=2"
 		CPPFLAGS=""
-		CFLAGS="-march=i686 -mtune=generic -O2 -pipe -fstack-protector-strong -fno-plt"
 		CFLAGS="-march=i686 -mtune=generic -O2 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection"
-		CXXFLAGS="-march=i686 -mtune=generic -O2 -pipe -fstack-protector-strong -fno-plt"
 		CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
 		LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now"
 		DEBUG_CFLAGS="-g -ggdb -fvar-tracking-assignments"
 		DEBUG_CXXFLAGS="-g -ggdb -fvar-tracking-assignments"
 	;;
 	x86_64)
-		CPPFLAGS="-D_FORTIFY_SOURCE=2"
 		CPPFLAGS=""
-		CFLAGS="-march=x86-64 -mtune=generic -O2 -pipe -fstack-protector-strong -fno-plt"
 		CFLAGS="-march=x86-64 -mtune=generic -O2 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection"
-		CXXFLAGS="-march=x86-64 -mtune=generic -O2 -pipe -fstack-protector-strong -fno-plt"
 		CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
 		LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now"
 		DEBUG_CFLAGS="-g -ggdb -fvar-tracking-assignments"
@@ -35,9 +29,12 @@ esac
 	--enable-maintainer-mode \
 	--enable-dependency-tracking \
 	CPPFLAGS="$CPPFLAGS" \
-	CFLAGS="$DEBUG_CFLAGS -Wall -Wextra -Werror -Wno-clobbered -Wno-deprecated-declarations $CFLAGS" \
-	CXXFLAGS="$DEBUG_CXXFLAGS -Wall -Wextra -Werror -Wno-clobbered -Wno-deprecated-declarations $CXXFLAGS" \
+	CFLAGS="$DEBUG_CFLAGS -Wall -Wextra -Werror -Wno-deprecated-declarations $CFLAGS" \
+	CXXFLAGS="$DEBUG_CXXFLAGS -Wall -Wextra -Werror -Wno-deprecated-declarations $CXXFLAGS" \
 	LDFLAGS="$LDFLAGS" \
 	DEBUG_CFLAGS="$DEBUG_CFLAGS" \
 	DEBUG_CXXFLAGS="$DEBUG_CXXFLAGS"
 
+# cscope target won't work without this
+#
+[ -f po/Makefile ] && echo -e '\n%:\n\t@:\n\n' >> po/Makefile
